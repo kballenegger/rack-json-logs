74959f6f96d9635d707fd50d229ed173f966ea83
Kenneth Ballenegger <kenneth@ballenegger.com>
2013-06-25 11:37:14 -0700

    initial commit / import

---
+++ lib/rack-json-logs.rb
@@ -0,0 +1,9 @@
+require "rack-json-logs/version"
+
+module Rack
+  module Json
+    module Logs
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack-json-logs/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Json
+    module Logs
+      VERSION = "0.0.1"
+    end
+  end
+end
